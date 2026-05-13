73bfe261198b927cc3163dce85d51c161e25edac
CAILLET Amelie <185417630+AmeLIE2024@users.noreply.github.com>
2026-05-13 15:52:33 +0200

    feat:burger menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
 import './App.css'
 import Button from './components/Button/Button'
+import Burger, { type NavLink } from './components/Burger/Burger'
 
 
 
 function App() {
 
   const handleClick = () => alert("Cliqué !");
+  const menu: NavLink[] = [{
+    id:1,label:"Se connecter",url:"/api/auth/login"
+  },{
+     id:2,label:"Créer une liste", url:"/api/wishlists/users/{userId}"
+  },{
+    id:3,label:"Mes listes", url:"/api/wishlists/users/{userId}"
+  }]
 
   return (
+    
     <>
+    <header>
+      <Burger links={menu}></Burger>
+    </header>
+    
       <Button name="Réserver" action={handleClick} />
+      
     </>
 
   )
