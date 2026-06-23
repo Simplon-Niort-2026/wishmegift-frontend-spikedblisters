27c6e676aa37a359104921bdbaca6f2b52775091
CAILLET Amelie <185417630+AmeLIE2024@users.noreply.github.com>
2026-06-23 15:33:36 +0200

    add header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,29 @@
+import "./Header.css";
+import Burger, { type NavLink } from "../Burger/Burger";
+
+interface HeaderProps {
+  logo: string;
+  links: NavLink[];
+}
+
+export default function Header({ logo, links = [] }: HeaderProps) {
+  return (
+    <header className="header">
+      <div className="header-inner">
+        <img src={logo} alt="Logo" className="header-logo" />
+        
+        <div className="burger-wrapper">
+          <Burger links={links} />
+        </div>
+
+        <nav className="header-nav">
+            {links.map((link) => (
+                <a key={link.id} href={link.url} className="nav-link">
+                     {link.label}
+                </a>
+            ))}
+        </nav>
+      </div>
+    </header>
+  );
+}
