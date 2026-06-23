deee85864d33a1d3822994a0e855632f8d99bda9
THIBAUD Ludovic <ludovic.thibaud@maif.fr>
2026-06-23 16:26:34 +0200

    add card wishlit mobile

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,10 @@ import { TitleCardWishlistMobile } from './components/TitleCardWishlistMobile/Ti
 import { BannerListDetailMobile } from './components/BannerListDetailMobile/BannerListDetailMobile';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import WishListCardMobile from "./components/WishListCardMobile/WishListCardMobile.tsx";
+
+
+
 
 
 
@@ -23,28 +27,37 @@ function App() {
   }]
 
   return (
-    
-    <>
-<Header logo={'/src/assets/logo wishmegift.png'} links={menu} >
-  
-</Header>
+
+      <>
+
+          <Header logo={'/src/assets/logo wishmegift.png'} links={menu}>
+
+          </Header>
 
 
-  
-    
-      <Button name="Réserver" action={handleClick} />
+          <header>
+              <Burger links={menu}></Burger>
+          </header>
 
-      <BannerListMobile title="Mes listes" date="13/05/2026" />
-      <TitleCardWishlistMobile title='Titre de la liste'/>
-      <StarRating />
 
-      <GiftCard name='fourchette' description='lorem' link='https://fr.wikipedia.org/wiki/Fourchette' price={79.99}/>
+          <Button name="Réserver" action={handleClick}/>
 
-      <BannerListDetailMobile title="Mes listes"/>
-      <BannerListDetailMobile title="Mes listes Archivées"/>
-      <Footer/>
-    </>
+          <BannerListMobile title="Mes listes" date="13/05/2026"/>
+          <TitleCardWishlistMobile title='Titre de la liste'/>
+          <StarRating/>
 
+          <GiftCard name='fourchette' description='lorem' link='https://fr.wikipedia.org/wiki/Fourchette'
+                    price={79.99}/>
+
+          <BannerListDetailMobile title="Mes listes"/>
+          <BannerListDetailMobile title="Mes listes Archivées"/>
+
+
+          <WishListCardMobile title={"Titre de la liste"} author={"Auteur"} description={"Description de la liste"}
+                              linkDetails={"Voir détail"} deleteButton={"X"}/>
+
+          <Footer/>
+      </>
   )
 }
 
