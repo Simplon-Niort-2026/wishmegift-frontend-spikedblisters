4ba29b9aff6dc49cd04446c7ad0f9c8184224224
DEHAINAULT Quentin <quentin.dehainault@maif.fr>
2026-06-25 08:59:12 +0200

    fix: Modification du React router dom en React router

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router";
 import App from "./App";
 import Homepage from "./pages/Homepage/Homepage";
 import './index.css'
